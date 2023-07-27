64af28c08eb3a30b0a3ad5b447030447726d76ef
wendymenez <wendy.silva00@gmail.com>
2023-07-27 16:56:52 -0300

    aluramidi completo

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom') .play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelectorAll('tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    //console.log(idAudio);
+
+    tecla.onclick  = function () {
+
+        tocaSom(idAudio);
+
+    };
+
+    contador = contador + 1;
+
+    //console.log(contador);
+
+}
 
 
 
